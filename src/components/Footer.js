afc02b6f01afc034da8dd074823e274c1f3cc352
Ben Dyson <benj.dyson@gmail.com>
2024-09-07 14:05:43 +0100

    Adding email and github logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap"
 import logo from '../assets/img/Logo.png';
-import NavIcon1 from '../assets/img/nav-icon1.svg';
-import NavIcon2 from '../assets/img/nav-icon2.svg';
-import NavIcon3 from '../assets/img/nav-icon3.svg';
+import linkedInLogo from '../assets/img/linkedInlogo.svg';
+import emailImg from '../assets/img/email.png';
+import githubLogo from '../assets/img/githubLogo.png';
 
 export const Footer = () => {
     return (
@@ -15,9 +15,9 @@ export const Footer = () => {
                     <Col sm={6} className="text-center">
                         <div className="social-footer">
                             <div className="social-icon">
-                                <a href="https://www.linkedin.com/in/ben-dyson-636717223/"><img src={NavIcon1} alt=""/></a>
-                                <a href="https://www.facebook.com/ben.dyson.754/"><img src={NavIcon2} alt=""/></a>
-                                <a href="https://www.instagram.com/ben_dyson_/"><img src={NavIcon3} alt=""/></a>
+                                <a href="https://github.com/Ben-Dyson-official"><img src={githubLogo} alt=""/></a>
+                                <a href="https://www.linkedin.com/in/ben-dyson-636717223/"><img src={linkedInLogo} alt=""/></a>
+                                <a href="mailto:benj.dyson@gmail.com"><img src={emailImg} alt=""/></a>
                             </div>
                             <p>CopyRight 2024. All rights reserved by Ben Dyson</p>
                         </div>
